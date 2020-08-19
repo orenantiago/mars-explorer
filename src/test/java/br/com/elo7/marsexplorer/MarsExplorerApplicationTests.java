2e2a5429ff42ec997872ba250f5c07ce13fbb3d7
Renan Silva <renan.tssilva@gmail.com>
2020-08-19 13:59:20 -0300

    big bang

---
+++ src/test/java/br/com/elo7/marsexplorer/MarsExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elo7.marsexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
