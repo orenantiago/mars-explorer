5b3edcc544c140b14782967a012cc283b3ec18b3
Renan Silva <renan.tssilva@gmail.com>
2020-08-20 17:12:40 -0300

    add error

--- src/main/java/br/com/elo7/marsexplorer/validation/Errors.java
+++ src/main/java/br/com/elo7/marsexplorer/validation/Errors.java
@@ -3,4 +3,5 @@ package br.com.elo7.marsexplorer.validation;
 public class Errors {
 
     public static MarsExplorerError ID_REQUIRED = new MarsExplorerError("id required");
+    public static MarsExplorerError PROBE_REQUIRED = new MarsExplorerError("probe required");
 }
